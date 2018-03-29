6b11fd99397c223a84247848ab5ac6ca4bd2e686
at-ducnguyen <duc.nguyen2@asiantech.vn>
2018-03-29 17:29:24 +0700

    Make View and Intent Exercises

--- app/src/at-ducnguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-ducnguyen/java/vn/asiantech/internship/MainActivity.java
@@ -1,13 +1,28 @@
 package vn.asiantech.internship;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String KEY_TITLE = "KEY_TITLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle("Android Training");
         setContentView(R.layout.activity_main);
+        final Button btnViewAndViewGroup = findViewById(R.id.btnViewAndViewGroup);
+        btnViewAndViewGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                String result = btnViewAndViewGroup.getText().toString();
+                intent.putExtra(KEY_TITLE, result);
+                startActivity(intent);
+            }
+        });
     }
 }
