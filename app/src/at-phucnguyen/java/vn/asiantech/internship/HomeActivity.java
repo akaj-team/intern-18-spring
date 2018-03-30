1a32b9185edd9d3c22a466b8eec9dd79094457ea
PhucNguyen <nguyenvanphuc0497@gmail.com>
2018-03-30 17:10:49 +0700

    Practic Intent and View View Group

---
+++ app/src/at-phucnguyen/java/vn/asiantech/internship/HomeActivity.java
@@ -0,0 +1,47 @@
+package vn.asiantech.internship;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button btnW1View = (Button) findViewById(R.id.btnW1View);
+        Button btnW1Intent = (Button) findViewById(R.id.btnW1Intent);
+
+        btnW1View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnW1Intent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, IntentActivity.class);
+
+                //Sử dụng bundle để gửi dữ liệu
+                Bundle bundle = new Bundle();
+                bundle.putString("keyTitle", "Bundle: ");
+                bundle.putString("keyMessege", "Đây là dữ liệu từ ActivityHome được gửi bằng Bundle!");
+                intent.putExtras(bundle);
+
+                //Sử dụng intent để gửi dữ liệu
+                intent.putExtra("keyTitleI", "Intent: ");
+                intent.putExtra("keyMessegeI", "Đây là dữ liệu từ ActivityHome được gửi bằng Intent!");
+
+                //khởi động Intent
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/at-phucnguyen/java/vn/asiantech/internship/IntentActivity.java
@@ -0,0 +1,31 @@
+package vn.asiantech.internship;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+
+        //Anh xa
+        TextView tvData = (TextView) findViewById(R.id.tvExtraData);
+        TextView tvDataI = (TextView) findViewById(R.id.tvExtraDataI);
+
+        //Lấy dữ liệu được gửi về bằng bundle đã tạo ở nơi gửi
+        Bundle extrasBundle = this.getIntent().getExtras();
+        String dataExtras = extrasBundle.getString("keyTitle") +
+                extrasBundle.getString("keyMessege");
+        tvData.setText(dataExtras);
+
+        //Lấy dữ liệu được gửi về bằng Intent đã tạo ở nơi gửi
+        Intent extrasIntent = this.getIntent();
+        String dataExtrasIntent = extrasIntent.getStringExtra("keyTitleI") +
+                extrasIntent.getStringExtra("keyMessegeI");
+        tvDataI.setText(dataExtrasIntent);
+    }
+}
