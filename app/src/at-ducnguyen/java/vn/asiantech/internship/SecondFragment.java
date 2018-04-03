dadf137666be23ea4e04ee806c8128eae574a4cf
duc.nguyen2 <duc.nguyen2@asiantech.vn>
2018-04-03 15:12:09 +0700

    PopBackStack from  SecondFragment to FirstFragment

--- app/src/at-ducnguyen/java/vn/asiantech/internship/SecondFragment.java
+++ app/src/at-ducnguyen/java/vn/asiantech/internship/SecondFragment.java
@@ -1,11 +1,14 @@
 package vn.asiantech.internship;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -13,16 +16,41 @@ import android.widget.TextView;
  */
 
 public class SecondFragment extends Fragment {
-    private TextView tvOutput;
+    private TextView mTvMessage;
+    private Button mBtnSend;
+    private EditText mEdtInput;
+    private SendStringOfSecondFragment data;
+
+    public interface SendStringOfSecondFragment {
+        public void sendMessageBack(String message);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            data = (SendStringOfSecondFragment) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must Implement SendStringOfSecondFragment ");
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_second, container, false);
-        tvOutput = (TextView) view.findViewById(R.id.tvOutput);
-        String result = getArguments().getString(SendDataActivity.KEY_INPUT);
-        tvOutput.setText(result);
+        mTvMessage = (TextView) view.findViewById(R.id.tvMessage);
+        String result = getArguments().getString(SendDataActivity.KEY_MESSAGE);
+        mTvMessage.setText(result);
+        mBtnSend = (Button) view.findViewById(R.id.btnSend);
+        mEdtInput = (EditText) view.findViewById(R.id.edtInput);
+        mBtnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data.sendMessageBack(mEdtInput.getText().toString());
+            }
+        });
         return view;
     }
 }
