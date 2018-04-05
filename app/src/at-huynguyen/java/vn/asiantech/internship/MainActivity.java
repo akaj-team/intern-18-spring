0cae1afb113af2f21e747486457717135f69b1db
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-05 16:59:14 +0700

    fix btn_comment

--- app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
@@ -3,19 +3,18 @@ package vn.asiantech.internship;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btncomment;
+    private Button btn_comment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btncomment = (Button) findViewById(R.id.btn_comment);
-        btncomment.setOnClickListener(new View.OnClickListener() {
+        btn_comment = (Button) findViewById(R.id.btn_comment);
+        btn_comment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
                 Intent intent = new Intent(MainActivity.this, ChatActivity.class);
