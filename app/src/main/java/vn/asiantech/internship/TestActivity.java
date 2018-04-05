d0543112b0e7b1b26ef0f73ff79457b4d2751652
vinhhuynhl.b <vinh.huynh@asiantech.vn>
2018-04-05 12:42:29 +0700

    fix ci

--- app/src/main/java/vn/asiantech/internship/TestActivity.java
+++ app/src/main/java/vn/asiantech/internship/TestActivity.java
@@ -2,7 +2,6 @@ package vn.asiantech.internship;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
 
