bfeb98a6c3250a9832bd545e695169a76e76a6cc
vinhhuynhl.b <vinh.huynh@asiantech.vn>
2018-04-05 12:07:07 +0700

    test CI

--- app/src/main/java/vn/asiantech/internship/TestActivity.java
+++ app/src/main/java/vn/asiantech/internship/TestActivity.java
@@ -2,12 +2,16 @@ package vn.asiantech.internship;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+
+    private TextView MYTEXTVIEW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MYTEXTVIEW.setText("VINH");
     }
 }
