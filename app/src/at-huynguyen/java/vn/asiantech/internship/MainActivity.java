6008826f7a7c004e585fa032b17ba7d490b6cfe3
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-05 16:45:05 +0700

    create view

--- app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
@@ -1,13 +1,32 @@
 package vn.asiantech.internship;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btncomment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btncomment = (Button) findViewById(R.id.btn_comment);
+        btncomment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+//    @Nullable
+//    @Override
+//    public void onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+//        View view = inflater.inflate(R.layout.activity_main,container,false);
+//        return view;
+//    }
