11793939ef105be38d2ab7a1c91a3d130d3f5674
hien.dao <hien.dao@asiantech.vn>
2018-04-06 15:29:18 +0700

    w1-view-fixed comment reviewer

--- app/src/at-hiendao/java/vn/asiantech/internship/MenuActivity.java
+++ app/src/at-hiendao/java/vn/asiantech/internship/MenuActivity.java
@@ -1,6 +1,5 @@
 package vn.asiantech.internship;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 
-@SuppressLint("Registered")
 public class MenuActivity extends Activity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,10 +21,6 @@ public class MenuActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, ViewActivity.class);
-                intent.putExtra("345", "Comment");
-                Bundle bundle = new Bundle();
-                bundle.putString("123", "123456789");
-                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
--- app/src/at-hiendao/java/vn/asiantech/internship/ViewActivity.java
+++ app/src/at-hiendao/java/vn/asiantech/internship/ViewActivity.java
@@ -1,28 +1,14 @@
 package vn.asiantech.internship;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class ViewActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_activity);
-        getIntentData();
-    }
-
-    private void getIntentData() {
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        EditText editText = findViewById(R.id.etCommentText);
-        editText.setText(bundle != null ? bundle.getString("123") : null);
-        TextView comment = findViewById(R.id.tvComment);
-        comment.setText(intent.getStringExtra("345"));
-
     }
 
 }
