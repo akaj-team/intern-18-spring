b5bcab55b5eb9178f8ee58bccfa93ba590fcd515
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-06 10:27:16 +0700

    fix file change

--- app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
@@ -2,27 +2,15 @@ package vn.asiantech.internship;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_comment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_comment = (Button) findViewById(R.id.btn_comment);
-        btn_comment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View view){
-                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
-                startActivity(intent);
             }
-        });
-    }
-}
+        }
 //    @Nullable
 //    @Override
 //    public void onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
