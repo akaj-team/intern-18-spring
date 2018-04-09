6a88857d9071668e7343171dd24e5e952331295a
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-09 16:57:16 +0700

    fix convention

--- app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
+++ app/src/at-huynguyen/java/vn/asiantech/internship/MainActivity.java
@@ -7,13 +7,13 @@ import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    final Button btn_comment = findViewById(R.id.btn_comment);
+    private Button mBtncomment = findViewById(R.id.btn_comment);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_comment.setOnClickListener(new View.OnClickListener() {
+        mBtncomment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
                 Intent intent = new Intent(MainActivity.this, ChatActivity.class);
