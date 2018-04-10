f34654e86efc6c9c5f7fa0b5694fe4be2b37ff24
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-10 10:35:09 +0700

    revert file

--- app/src/at-huynguyen/java/vn/asiantech/internship/HomeActivity.java
+++ app/src/at-huynguyen/java/vn/asiantech/internship/HomeActivity.java
@@ -6,17 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     private Button mBtnComment = findViewById(R.id.btnComment);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         mBtnComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
-                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                Intent intent = new Intent(HomeActivity.this, ChatActivity.class);
                 startActivity(intent);
             }
         });
