90d1e9602763e268df6436ea37c6330d9259464d
hien.dao <hien.dao@asiantech.vn>
2018-04-11 16:40:18 +0700

    remove package java lang

--- app/src/at-hiendao/java/vn/asiantech/internship/ListFriendAdapter.java
+++ app/src/at-hiendao/java/vn/asiantech/internship/ListFriendAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.lang.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
