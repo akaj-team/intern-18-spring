5d6072c14a1bb4ce8ce9e48f0104fe16ee4f5d90
duc.nguyen2 <duc.nguyen2@asiantech.vn>
2018-04-12 17:38:29 +0700

    [Create] viewpager and add library indicator

---
+++ app/src/at-ducnguyen/java/vn/asiantech/internship/ListFriendFragment.java
@@ -0,0 +1,17 @@
+package vn.asiantech.internship;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFriendFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_friend, container, false);
+        return view;
+    }
+}
---
+++ app/src/at-ducnguyen/java/vn/asiantech/internship/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package vn.asiantech.internship;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ListFriendFragment();
+            case 1:
+                return new ListFriendRequestFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "List Friend";
+            case 1:
+                return "List Friend Request";
+            default:
+                return null;
+        }
+    }
+}
