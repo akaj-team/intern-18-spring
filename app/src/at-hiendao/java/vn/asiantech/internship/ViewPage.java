8937b7ddeaf1bc92044d4b85cd036d6e67328b60
hien.dao <hien.dao@asiantech.vn>
2018-04-13 10:04:37 +0700

    create viewpage

---
+++ app/src/at-hiendao/java/vn/asiantech/internship/ViewPage.java
@@ -0,0 +1,15 @@
+package vn.asiantech.internship;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+public class ViewPage extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpage);
+
+    }
+}
