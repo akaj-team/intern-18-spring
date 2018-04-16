c87658116d023a3e949a80f04628c8427d0241af
at-huynguyen3 <huy.nguyen3@asiantech.vn>
2018-04-16 15:40:29 +0700

    create ui

---
+++ app/src/at-huynguyen/java/vn/asiantech/internship/MainFragment.java
@@ -0,0 +1,20 @@
+package vn.asiantech.internship;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainFragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+    }
+
+    public void showText(String nameText, String sexText) {
+        ShowTextFragment showTextFragment
+                = (ShowTextFragment) this.getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentShowText);
+        showTextFragment.showText(nameText, sexText);
+    }
+}
